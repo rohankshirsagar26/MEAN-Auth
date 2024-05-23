b017e9dd05cd675c62d6933ef24334f2ec35796f
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-05-23 10:36:50 +0530

    Implemented dotenv packe to store variables

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://localhost:27017/AuthDB');
+        const connection = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log(`Connected to database ${connection.connection.name}`);
     } catch (error) {
         console.log(`Error connecting to database ${connection.connection.name}: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
 const connectDB = require('./db/db');
 
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server started on ${3000}`);
+    console.log(`Server started on ${process.env.PORT}`);
     connectDB();
 })
