b98111ea259fe07024cb7d1cdfbe20f7f4305b0a
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-05-23 11:34:28 +0530

    User model created

---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImage: {
+        type: String,
+        required: false,
+        default: 'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png'
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
