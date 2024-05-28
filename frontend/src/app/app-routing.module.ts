5d40b363d7acca9e49209f589ad180c71363852c
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-05-28 12:03:40 +0530

    Created routing and various components and added routes for login and register page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  {
+    path: 'forget-password',
+    component: ForgetPasswordComponent,
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
