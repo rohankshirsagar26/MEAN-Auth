a8e8673041d1027bc78957cedf556640d1159b64
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-02 19:53:07 +0530

    Reset email endpoint created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, registerAdmin } = require('../controllers/auth');
+const { register, login, registerAdmin, sendEmail } = require('../controllers/auth');
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/register-admin', registerAdmin);
+authRouter.post('/send-email', sendEmail);
 
 module.exports = authRouter;
