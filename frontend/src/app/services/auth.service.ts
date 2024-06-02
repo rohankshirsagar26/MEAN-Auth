365985a4ca4d84cd73512e44af55ffa2cb34670e
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-02 11:25:27 +0530

    Created register endpoint

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { apiUrls } from '../api.urls';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  registerService(registerObj: any) {
+    return this.http.post<any>(`${apiUrls.authServiceApi}login`, registerObj);
+  }
 }
