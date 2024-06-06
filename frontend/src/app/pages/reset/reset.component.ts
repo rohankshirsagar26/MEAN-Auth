dd8b846e6f6872eb5113a6eed085af40c303d639
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-06 18:39:00 +0530

    Reset password markup created

--- frontend/src/app/pages/reset/reset.component.ts
+++ frontend/src/app/pages/reset/reset.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.css']
+  styleUrls: ['./reset.component.css'],
 })
 export class ResetComponent implements OnInit {
+  resetForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  resetPassword() {}
 }
