cc79bd963c74057541402864610ecc1f1c177d21
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-07 16:44:46 +0530

    Cleared loca lstorage on logout

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe((res) => {
+      this.isLoggedIn = this.authService.isLoggedIn();
+    });
   }
 
+  logout() {
+    this.toastr.success('Logged out successfully', 'Success');
+    localStorage.clear();
+    this.authService.isLoggedIn$.next(false);
+  }
 }
