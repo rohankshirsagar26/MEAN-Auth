1f5cce1c5886e99aea5c4b4a47afe70fc96215b2
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-07 09:28:17 +0530

    Renamed send email method

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { register, login, registerAdmin, resetPassword } = require('../controllers/auth');
+const { register, login, registerAdmin, sendEmail } = require('../controllers/auth');
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/register-admin', registerAdmin);
-authRouter.post('/reset-password', resetPassword);
+authRouter.post('/reset-password', sendEmail);
 
 module.exports = authRouter;
