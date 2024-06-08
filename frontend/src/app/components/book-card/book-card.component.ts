8d06ede85d2325d6c2deef4b63ce308af43274a7
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2024-06-08 17:59:47 +0530

    Created and rendered book card component in home component

--- frontend/src/app/components/book-card/book-card.component.ts
+++ frontend/src/app/components/book-card/book-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.css']
+  styleUrls: ['./book-card.component.css'],
 })
 export class BookCardComponent implements OnInit {
+  @Input() book: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
